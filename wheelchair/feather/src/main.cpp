1ebc23d0665648164b63f18add2c80f05818e39e
Carlos Precioso <kalprestito@gmail.com>
2019-03-25 17:06:35 +0100

    Include GPS characteristic

--- wheelchair/feather/src/main.cpp
+++ wheelchair/feather/src/main.cpp
@@ -1,15 +1,15 @@
 /*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
+  This is an example for our nRF51822 based Bluefruit LE modules
 
- Pick one up today in the adafruit shop!
+  Pick one up today in the adafruit shop!
 
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
+  MIT license, check LICENSE for more information
+  All text above, and the splash screen below must be included in
+  any redistribution
 *********************************************************************/
 
 /*
@@ -26,6 +26,7 @@
 #include <Adafruit_BLE.h>
 #include <Adafruit_BluefruitLE_SPI.h>
 #include <Adafruit_BluefruitLE_UART.h>
+#include <Adafruit_GPS.h>
 
 #if SOFTWARE_SERIAL_AVAILABLE
 #include <SoftwareSerial.h>
@@ -39,6 +40,9 @@
 // Create the Bluefruit object for Feather 32u4
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
+// Create Adafruit GPS
+Adafruit_GPS GPS(&Serial1);
+
 // BNO settings
 #define BNO055_SAMPLERATE_DELAY_MS (200)
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
@@ -47,6 +51,7 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 int32_t imuServiceId;
 int32_t orientationCharId;
 int32_t calibrationCharId;
+int32_t locationCharId;
 
 // A small helper
 void error(const __FlashStringHelper *err)
@@ -72,6 +77,24 @@ void initSensor(void)
   bno.setExtCrystalUse(true);
 }
 
+// Initializes GPS
+void initGPS(void) {
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+
+  // Turn on only the "minimum recommended" data for high update rates!
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ);   // 200 MHz update rate
+  // For the parsing code to work nicely and have time to sort through the data, and
+  // print it out using anything higher than 1 Hz is not suggested
+
+  delay(1000);
+  // Ask for firmware version
+  Serial1.println(PMTK_Q_RELEASE);
+}
+
 // Sets up the HW an the BLE module (this function is called
 // automatically on startup)
 void setup(void)
@@ -94,6 +117,9 @@ void setup(void)
   // Setup the BNO055 sensor
   initSensor();
 
+  // Setup the GPS
+  initGPS();
+
   // Perform a factory reset to make sure everything is in a known state
   if (!ble.factoryReset())
   {
@@ -108,7 +134,7 @@ void setup(void)
   ble.verbose(true);
 
   // Change the device name to fit its purpose
-  if (!ble.sendCommandCheckOK(F("AT+GAPDEVNAME=MartiTooth")))
+  if (!ble.sendCommandCheckOK(F("AT+GAPDEVNAME=NaturelyBT")))
   {
     error(F("Could not set device name."));
   }
@@ -139,6 +165,17 @@ void setup(void)
   }
 
   // Add the Calibration Service to the advertising data
+  // (needed for Nordic apps to detect the service)
+  ble.sendCommandCheckOK(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
+
+  // Add the Location characteristic
+  success = ble.sendCommandWithIntReply(F("AT+GATTADDCHAR=UUID128=BF-EF-DE-AD-44-57-66-77-88-A9-AA-BB-CC-DD-EE-FF,PROPERTIES=0x10,MIN_LEN=1,MAX_LEN=17,VALUE=\"\""), &locationCharId);
+  if (!success)
+  {
+    error(F("Could not add Location characteristic."));
+  }
+
+  // Add the Location Service to the advertising data
   // (needed for Nordic apps to detect the service)
   ble.sendCommandCheckOK(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
 
@@ -206,10 +243,48 @@ void orientation()
   ble.println(String(rotZ));
 }
 
+
+uint32_t locationTimer = millis();
+void location() {
+  // read data from the GPS in the 'main loop'
+  GPS.read();
+
+  // if a sentence is received, we can check the checksum, and then parse it...
+  if (GPS.newNMEAreceived()) {
+    // If we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (locationTimer > millis())  locationTimer = millis();
+
+  // approximately every 2 seconds or so, print out the current GPS stats
+  if (millis() - locationTimer > 5000) {
+    locationTimer = millis();
+
+    // if (!GPS.fix) return
+
+    // Command is sent when \n (\r) or println is called
+    // AT+GATTCHAR=CharacteristicID,value
+    ble.print(F("AT+GATTCHAR="));
+    ble.print(locationCharId);
+    ble.print(F(","));
+    ble.print(GPS.latitudeDegrees, 4);
+    ble.print(F(","));
+    ble.println(GPS.longitudeDegrees, 4);
+  }
+}
+
 void loop(void)
 {
 
   orientation();
+  location();
 
   // Check if command executed OK
   if (!ble.waitForOK())
