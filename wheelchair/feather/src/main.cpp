16ccaba7ccc3cdea336280a670e5edcb1c7f0068
Carlos Precioso <kalprestito@gmail.com>
2019-04-03 16:17:45 +0200

    Feather just for heart rate

--- wheelchair/feather/src/main.cpp
+++ wheelchair/feather/src/main.cpp
@@ -18,15 +18,9 @@
 */
 
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
 #include <Adafruit_BLE.h>
 #include <Adafruit_BluefruitLE_SPI.h>
 #include <Adafruit_BluefruitLE_UART.h>
-#include <Adafruit_GPS.h>
 
 #include "BluefruitConfig.h"
 
@@ -37,18 +31,13 @@
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ,
                              BLUEFRUIT_SPI_RST);
 
-// Create Adafruit GPS
-Adafruit_GPS GPS(&Serial1);
-
-// BNO settings
-#define BNO055_SAMPLERATE_DELAY_MS (200)
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+const auto heartSensorPin =
+    A0;                   // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+const int delayMs = 250;  // delay for half a second
 
 // GATT service information
 int32_t imuServiceId;
-int32_t orientationCharId;
-int32_t calibrationCharId;
-int32_t locationCharId;
+int32_t heartRateCharId;
 
 // A small helper
 void error(const __FlashStringHelper *err) {
@@ -62,54 +51,7 @@ void error(const __FlashStringHelper *err) {
 }
 
 // Initializes BNO055 sensor
-void initSensor(void) {
-  if (!bno.begin()) {
-    error(F("No BNO055 detected. Check your wiring or I2C ADDR!"));
-  }
-  delay(1000);
-  bno.setExtCrystalUse(true);
-}
-
-// Keeps track of whether we're using the interrupt
-// off by default!
-boolean usingInterrupt = false;
-
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-    usingInterrupt = true;
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-    usingInterrupt = false;
-  }
-}
-
-// Initializes GPS
-void initGPS(void) {
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
-
-  // Turn on only the "minimum recommended" data for high update rates!
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ);  // 200 MHz update rate
-  // For the parsing code to work nicely and have time to sort through the data,
-  // and print it out using anything higher than 1 Hz is not suggested
-
-  delay(1000);
-  // Ask for firmware version
-  Serial1.println(PMTK_Q_RELEASE);
-
-  // This code can have a timer0 interrupt go off
-  // every 1 millisecond, and read data from the GPS. That makes the
-  // loop code  a lot easier!
-  useInterrupt(true);
-}
+void initSensor(void) { pinMode(heartSensorPin, INPUT); }
 
 void initBluetooth() {
   boolean success;
@@ -144,45 +86,19 @@ void initBluetooth() {
                                     "55-66-77-88-99-AA-BB-CC-DD-EE-FF"),
                                   &imuServiceId);
   if (!success) {
-    error(F("Could not add Orientation service."));
+    error(F("Could not add Heart Rate service."));
   }
 
   // Add the Orientation characteristic
   success = ble.sendCommandWithIntReply(
       F("AT+GATTADDCHAR=UUID128=DE-AD-BE-EF-44-55-66-77-88-99-AA-BB-CC-DD-EE-"
         "FF,PROPERTIES=0x10,MIN_LEN=1,MAX_LEN=17,VALUE=\"\""),
-      &orientationCharId);
+      &heartRateCharId);
   if (!success) {
-    error(F("Could not add Orientation characteristic."));
+    error(F("Could not add Heart Rate characteristic."));
   }
 
   // Add the Orientation Service to the advertising data
-  // (needed for Nordic apps to detect the service)
-  ble.sendCommandCheckOK(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
-
-  // Add the Calibration characteristic
-  success = ble.sendCommandWithIntReply(
-      F("AT+GATTADDCHAR=UUID128=BE-EF-DE-AD-44-55-66-77-88-99-AA-BB-CC-DD-EE-"
-        "FF,PROPERTIES=0x10,MIN_LEN=1,MAX_LEN=17,VALUE=\"\""),
-      &calibrationCharId);
-  if (!success) {
-    error(F("Could not add Calibration characteristic."));
-  }
-
-  // Add the Calibration Service to the advertising data
-  // (needed for Nordic apps to detect the service)
-  ble.sendCommandCheckOK(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
-
-  // Add the Location characteristic
-  success = ble.sendCommandWithIntReply(
-      F("AT+GATTADDCHAR=UUID128=BF-EF-DE-AD-44-57-66-77-88-A9-AA-BB-CC-DD-EE-"
-        "FF,PROPERTIES=0x10,MIN_LEN=1,MAX_LEN=17,VALUE=\"\""),
-      &locationCharId);
-  if (!success) {
-    error(F("Could not add Location characteristic."));
-  }
-
-  // Add the Location Service to the advertising data
   // (needed for Nordic apps to detect the service)
   ble.sendCommandCheckOK(F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18"));
 
@@ -196,114 +112,44 @@ void setup(void) {
   delay(500);
 
   // Set LED error flag
-
-  pinMode(LED_PIN, OUTPUT);
-  analogWrite(LED_PIN, LOW);
   Serial.begin(115200);
 
-  // Setup the BNO055 sensor
+  // Setup the sensor
   initSensor();
-
-  // Setup the GPS
-  initGPS();
 
   // Setup the Bluetooth
   initBluetooth();
 }
 
-// Interrupt is called once a millisecond, looks for any new GPS data, and
-// stores it
-SIGNAL(TIMER0_COMPA_vect) { char c = GPS.read(); }
-
-bool isCalibrated() {
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;  // variable for gyroscope, accelerometer
-                                     // and magnetometer
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  // Command is sent when \n (\r) or println is called
-  // AT+GATTCHAR=CharacteristicID,value
-  ble.print(F("AT+GATTCHAR="));
-  ble.print(calibrationCharId);
-  ble.print(F(","));
-  ble.print(String(system));
-  ble.print(F(","));
-  ble.print(String(gyro));
-  ble.print(F(","));
-  ble.print(String(accel));
-  ble.print(F(","));
-  ble.println(String(mag));
-
-  if (gyro >= 2 && mag >= 2) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-void orientation() {
-  if (!isCalibrated()) {
-    return;
-  }
-
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  float rotX = event.orientation.x;
-  float rotY = event.orientation.y;
-  float rotZ = event.orientation.z;
-
-  // Command is sent when \n (\r) or println is called
-  // AT+GATTCHAR=CharacteristicID,value
-  ble.print(F("AT+GATTCHAR="));
-  ble.print(orientationCharId);
-  ble.print(F(","));
-  ble.print(String(rotX));
-  ble.print(F(","));
-  ble.print(String(rotY));
-  ble.print(F(","));
-  ble.println(String(rotZ));
-}
-
-void location() {
-  // if a sentence is received, we can check the checksum, and then parse it...
-  if (GPS.newNMEAreceived()) {
-    // If we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    // Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived()
-    // flag to false
-
-    if (!GPS.parse(GPS.lastNMEA()))  // this also sets the newNMEAreceived()
-                                     // flag to false
-      return;  // we can fail to parse a sentence in which case we should just
-               // wait for another
-
-    if (!GPS.fix) return;
-
-    // Command is sent when \n (\r) or println is called
-    // AT+GATTCHAR=CharacteristicID,value
-    ble.print(F("AT+GATTCHAR="));
-    ble.print(locationCharId);
-    ble.print(F(","));
-    ble.print(GPS.latitudeDegrees, 4);
-    ble.print(F(","));
-    ble.println(GPS.longitudeDegrees, 4);
-  }
-}
-
 void loop(void) {
-  orientation();
-  location();
+  int heartRate = analogRead(heartSensorPin);
 
-  // Check if command executed OK
-  if (!ble.waitForOK()) {
-    error(F("Failed to get response!"));
+  static const int numValues = 10;
+  static int lastValues[numValues];
+  static int currentValue = 0;
+
+  lastValues[currentValue] = heartRate;
+  currentValue = (currentValue + 1) % numValues;
+
+  static auto lastTime = millis();
+  if (millis() - lastTime >= 1000) {
+    lastTime = millis();
+
+    int average = 0;
+    for (int i = 0; i < numValues; i++) average += lastValues[i];
+    average /= numValues;
+
+    ble.print(F("AT+GATTCHAR="));
+    ble.print(heartRateCharId);
+    ble.print(F(","));
+    ble.println(String(average));
+
+    // Check if command executed OK
+    if (!ble.waitForOK()) {
+      error(F("Failed to get response!"));
+    }
   }
 
   // Delay before next measurement update
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  delay(delayMs);
 }
